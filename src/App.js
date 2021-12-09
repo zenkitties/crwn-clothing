92a22d579e3b9b81b37c457bd7f23c0774066da7
Nick Myers <zenfootphilosopher@gmail.com>
2021-12-08 18:41:58 -0800

    setting up firebase part one

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component.jsx';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       < Header />
       <Route exact path='/' component={HomePage} />
       <Route path='/shop' component={ShopPage} />
+      <Route path='/signin' component={SignInAndSignUpPage} />
     </div>
   )
 }
